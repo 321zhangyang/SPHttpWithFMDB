f4f50d11a50a4a247da772401af8523b2042bcf2
换一换 <huanyihuan@huanyihuandeMacBook-Pro.local>
2016-03-08 14:45:23 +0800

    @网络封装基于AF3.0

--- SPHttp/ViewController.m
+++ SPHttp/ViewController.m
@@ -7,16 +7,66 @@
 //
 
 #import "ViewController.h"
-
+#import "SPHttp.h"
+#define baseUrl @"http://api.beikeshushe.com/gateway.php"
+#define ImageKey @"avatar"
 @interface ViewController ()
-
+@property (nonatomic, strong) NSMutableArray *dataArray;
+@property (nonatomic, strong) UITableView *myTabelView;
 @end
 
 @implementation ViewController
-
+-(NSMutableArray *)dataArray
+{
+    if (!_dataArray) {
+        _dataArray = [NSMutableArray array];
+    }
+    return _dataArray;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+  
+  
+    
+    [self request];
+    
+}
+
+
+-(void)request
+{
+    
+    
+    
+    NSDictionary *dic = @{@"method":@"readgroup.recommend_read_group",@"uid":@"33",@"p":@"1"};
+    
+
+    
+    [SPHttp postREquestCacheUrlStr:baseUrl withDic:dic success:^(NSDictionary *requestDic, NSString *msg) {
+       
+    } failure:^(NSString *errorInfo) {
+        
+    }];
+    
+
+}
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+
+{NSLog(@"1111");
+    
+    if ([keyPath isEqualToString:@"fractionCompleted"] && [object isKindOfClass:[NSProgress class]]) {
+        
+        NSProgress *progress = (NSProgress *)object;
+        
+        NSLog(@"%@",progress);
+        
+        
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
